d664c2e7d8a74807b771cccad75525e3778cc827
jikkev <s1198343@student.windesheim.nl>
2025-10-02 11:55:49 +0200

    implementing Get Product ID function in BoughtproductService

--- Grocery.Core/Services/BoughtProductsService.cs
+++ Grocery.Core/Services/BoughtProductsService.cs
@@ -18,9 +18,20 @@ namespace Grocery.Core.Services
             _clientRepository=clientRepository;
             _productRepository=productRepository;
         }
-        public List<BoughtProducts> Get(int? productId)
+        public List<BoughtProducts> Get(int productId)
         {
-            throw new NotImplementedException();
+            List<GroceryListItem> groceryListItems = _groceryListItemsRepository.GetAll().Where(g => g.ProductId == productId).ToList();
+            List<BoughtProducts> boughtProducts = new List<BoughtProducts>();
+
+            foreach (GroceryListItem item in groceryListItems)
+            {
+                GroceryList groceryList = _groceryListRepository.Get(item.GroceryListId);
+                Client client = _clientRepository.Get(groceryList.ClientId);
+                Product product = _productRepository.Get(item.ProductId);
+                boughtProducts.Add(new BoughtProducts(client, groceryList, product));
+            }
+
+            return boughtProducts;
         }
     }
 }
